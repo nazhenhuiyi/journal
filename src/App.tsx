6e5dc367a8f6f58f719a039eb3729d9f8dc120db
zilin-unifra <zilin@unifra.io>
2026-04-25 16:29:30 +0800

    Add hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { MotionConfig } from 'motion/react'
+import { HashRouter, Navigate, Route, Routes } from 'react-router'
 import MarkdownPreviewPage from './pages/MarkdownPreviewPage'
 
 function App() {
   return (
     <MotionConfig reducedMotion="user">
-      <MarkdownPreviewPage />
+      <HashRouter>
+        <Routes>
+          <Route element={<Navigate to="/preview" replace />} index />
+          <Route element={<MarkdownPreviewPage />} path="/preview" />
+          <Route element={<Navigate to="/preview" replace />} path="*" />
+        </Routes>
+      </HashRouter>
     </MotionConfig>
   )
 }
